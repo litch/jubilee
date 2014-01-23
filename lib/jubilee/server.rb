ba0b12ad8b7f1c51876fd84b79c7994052827aa9
isaiah <issaria@gmail.com>
2014-01-23 23:50:29 +0100

    #2 pass configurations to server verticle

--- lib/jubilee/server.rb
+++ lib/jubilee/server.rb
@@ -1,12 +1,21 @@
-require 'rack/methodoverride'
 module Jubilee
-  class Server < VertxServer
+  class Server < PlatformManager
     def initialize(app, opts = {})
       options = {Host: "0.0.0.0", Port: 8080, ssl: false}.merge(opts)
       if (options[:ssl]) && options[:ssl_keystore].nil?
           raise ArgumentError, "Please provide a keystore for ssl"
       end
-      super(Application.new(app), options)
+      # back compatible
+      if app
+        options[:rackapp] = app
+      end
+      super(options)
+    end
+
+    def start
+    end
+
+    def stop
     end
   end
 end
